2a3061bc604ed2054649bfc087ebe7e462b21062
smashfligaeta <dgrayson11@gmail.com>
2015-03-07 21:02:43 -0700

    COPTER IN YO FACE!

--- copterface.js
+++ copterface.js
@@ -15,6 +15,7 @@ var lastPng;
 var navData;
 var flying = false;
 var finalPng;
+var hitCounter = 0;
 var win = new cv.NamedWindow("bears", 10000);
 var startTime = new Date().getTime();
 var log = function(s){
@@ -26,57 +27,106 @@ var time = ( ( new Date().getTime() - startTime ) / 1000 ).toFixed(2);
 pngStream
   .on('error', console.log)
   .on('data', function(pngBuffer) {
-    //console.log("got image");
     lastPng = pngBuffer;
     detectFaces();
   });
      
-var processingImage = false;
+
 var detectFaces = function() {
-  console.log("Detecting?");
-  console.log(processingImage); 
-  if( ( ! processingImage ) && lastPng ){
-    cv.readImage( lastPng, function(err, im) {
-      var opts = {};
-      im.detectObject(cv.FACE_CASCADE, opts, function(err, faces) {
+  if (flying == true){
+    if( ( ! processingImage ) && lastPng ){
+      processingImage = true;
+      cv.readImage( lastPng, function(err, im) {
+        var opts = {};
+        im.detectObject(cv.FACE_CASCADE, opts, function(err, faces) {
 
-        var face;
-        var biggestFace;
+          var face;
+          var biggestFace;
 
-        for(var k = 0; k < faces.length; k++) {
-          console.log("look a face!")
-          face = faces[k];
-          if( !biggestFace || biggestFace.width < face.width ) biggestFace = face;
-        }
-        if (biggestFace){
-          im.ellipse(biggestFace.x + biggestFace.width/2, biggestFace.y + biggestFace.height/2, biggestFace.width/2, biggestFace.height/2);
-        }
-        win.show(im);
-        processingImage = false;
+          for(var k = 0; k < faces.length; k++) {
+            face = faces[k];
+            if( !biggestFace || biggestFace.width < face.width ) biggestFace = face;
+          }
+          if (biggestFace){
+            correct(biggestFace, im);
+            if (biggestFace.height > im.height()/4 || biggestFace.width > im.width()/4){
+              client.stop()
+              client.after(100, function() {
+                client.back(.5);
+              }).after(500, function() {
+                client.stop();
+              })
+              spin();
+            }else{
+              client.stop();
+              client.front(.25);
+            }
+            im.ellipse(biggestFace.x + biggestFace.width/2, biggestFace.y + biggestFace.height/2, biggestFace.width/2, biggestFace.height/2);
+          }else{
+            client.stop();
+            spin();
+          }
+          //win.show(im);
+          processingImage = false;
 
-      }, opts.scale, opts.neighbors, opts.min && opts.min[0], opts.min && opts.min[1]);
-    });
+        }, opts.scale, opts.neighbors, opts.min && opts.min[0], opts.min && opts.min[1]);
+      });
+    }else if (hitCounter == 10{
+      spin();
+    }else{
+      hitCounter++;
+    }
   }
 };
 
-//var faceInterval = setInterval(detectFaces, 150);
+var spin = function() {
+  client.stop()
+  client.clockwise(.25);
+}
 
-/*client.takeoff();
+var correct = function(face, im) {
+    var faceCenterX = face.width/2;
+    var faceCenterY = face.height/2;
+    if (faceCenterX > im.width()/2){
+      client.right(.25)
+      client.after(500, function(){
+        client.stop();
+      })
+    }else if (faceCenterX < im.width()/2){
+      client.left(.25)
+      client.after(500, function(){
+        client.stop();
+      })
+    }if (faceCenterY > im.height()/2){
+      client.up(.25)
+      client.after(500, function(){
+        client.stop();
+      })
+    }else if (faceCenterY > im.height()/2){
+      client.down(.25)
+      client.after(500, function(){
+        client.stop();
+      })
+    }
+}
+
+var faceInterval;
+
+client.takeoff();
 client.after(5000,function(){ 
   log("going up");
-  this.up(1);
-}).after(1000,function(){ 
+  this.up(.25);
+}).after(5000,function(){ 
   log("stopping");
-  this.stop(); 
+  this.stop();
   flying = true;
 });
-
 
 client.after(30000, function() {
     flying = false;
     this.stop();
     this.land();
-  });*/
+  });
 
 client.on('navdata', function(navdata) {
   navData = navdata;

66914c71a5dc261a98f5d3bcded1ee45f6797ae6
Conner Swann <me@connerswann.me>
2015-03-08 02:34:19 -0700

    added logic and comments

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ var bodyparser = require('body-parser');
 //http server
 var http = require('http');
 var server = http.createServer(app);
+var cv = require('opencv');
+
 
 //opens up access to TCP stream 
 //var stream = require("dronestream");
 //opens up the drone API to control
 var arDrone = require('ar-drone');
-var drone = arDrone.createClient();
+var client = arDrone.createClient();
 
-var cv = require('opencv');
+//flag that determines if the drone is flying
+var flying = false;
+
 //static files
 app.use(express.static(__dirname + '/public'));
 
@@ -31,19 +35,19 @@ app.post('/', function(req, res) {
 });
 // makes drone takeoff, turn clockwise, and land
 app.post('/takeoff', function(req, res) {
-    drone.takeoff()
+    client.takeoff()
 });
 
 app.post('/clockwise', function(req, res) {
-    drone.clockwise(0.25);
+    client.clockwise(0.25);
 });
 
 app.post('/stop', function(req, res) {
-    drone.stop();
+    client.stop();
 });
 
 app.post('/land', function(req, res) {
-    drone.land();
+    client.land();
 });
 
 app.post('/facefinder', function(req, res) {
@@ -53,18 +57,17 @@ app.post('/facefinder', function(req, res) {
     var missCounter = MISSES;
 
     //drone client API
-    var client = arDrone.createClient();
+    //var client = arDrone.createClient();
     //open image stream from drone
     var pngStream = client.getPngStream();
     var lastPng;
     //flag to prevent two image processing events
     var processingImage = false;
-    //flag that determines if the drone is flying
-    var flying = false;
     //flag that determines if the drone is searching
     var searching = false;
     //flight time recorder
     var startTime = new Date().getTime();
+    var win = new cv.NamedWindow('Face Feed', 0);
     var log = function(s) {
     var time = ((new Date().getTime() - startTime ) / 1000 ).toFixed(2);
       console.log(time + ' \t'+s);
@@ -79,149 +82,161 @@ app.post('/facefinder', function(req, res) {
       });
 
     //correct trim
+    log("Correct Trim");
     client.ftrim();
 
+    log("Battery: " + client.battery());
     //setup mission
+    log("Taking Off");
     client.takeoff();
-    log(client.battery());
+    
     client.after(5000, function() {
       log('going up');
       this.up(.25);
-    }).after(5000, function() {
+    }).after(2000, function() {
       log('stopping');
       this.stop();
       flying = true;
     });
 
-    client.after(30000, function() {
-        flying = false;
-        this.stop();
-        this.land();
-    });
-});
+    // client.after(30000, function() {
+    //     flying = false;
+    //     this.stop();
+    //     this.land();
+    // });
 
-
-// detect faces and react to them
-var detectFaces = function() {
-  if (flying == true) {
-    if ((!processingImage) && lastPng ) {
-      //tell the world we're doing something
-      processingImage = true;
-      cv.readImage(lastPng, function(err, im) {
-        //save image to window
-        win.show(im);
-        var opts = {};
-        im.detectObject(cv.FACE_CASCADE, opts, function(err, faces) {
-          //current face
-          var face;
-          //biggest face in current frame
-          var biggestFace;
-
-          //iterate over all faces and find largest
-          for (var k = 0; k < faces.length; k++) {
-            face = faces[k];
-            // if this face is bigger, save it
-            if (!biggestFace || biggestFace.width < face.width) biggestFace = face;
-          }
-          // if there is at least one face detected
-          if (biggestFace) {
-            // write the ellipse to the stream
-            im.ellipse(biggestFace.x + biggestFace.width / 2, biggestFace.y + biggestFace.height/2, biggestFace.width/2, biggestFace.height/2);
-            // and render it
+    // detect faces and react to them
+    var detectFaces = function() {
+      if (flying == true) {
+        if ((!processingImage) && lastPng ) {
+          //tell the world we're doing something
+          processingImage = true;
+          cv.readImage(lastPng, function(err, im) {
+            //save image to window
             win.show(im);
-            // count down frames with no face
-            missCounter = MISSES;
-            // attempt to correct position
-            correct(biggestFace, im);
-            // if the face's height/width > 1/6 the total image height/width 
-            if (biggestFace.height > im.height() / 6 || biggestFace.width > im.width()/6) {
-              // halt movement
-              client.stop();
-              client.after(100, function() {
-                // back up real quick
-                client.back(.25);
-              }).after(500, function() {
-                // halt movement
-                client.stop();
-              });
-            }
-            // else, face isn't that close, CHASE!!
-            else {
-              client.stop();
-              client.front(.1);
-            }
-          }
-          // if we haven't seen a face for 10 consecutive frames
-          else if (missCounter == 0) {
-            // search
-            srch();
-          }
-          // we still have hope we'll find that face again
-          else {
-            // decrement the counter
-            missCounter--;
-          }
-          // done processing
-          processingImage = false;
+            var opts = {};
+            im.detectObject(cv.FACE_CASCADE, opts, function(err, faces) {
+              //current face
+              var face;
+              //biggest face in current frame
+              var biggestFace;
 
-        }, opts.scale, opts.neighbors, opts.min && opts.min[0], opts.min && opts.min[1]);
+              //iterate over all faces and find largest
+              for (var k = 0; k < faces.length; k++) {
+                face = faces[k];
+                // if this face is bigger, save it
+                if (!biggestFace || biggestFace.width < face.width) biggestFace = face;
+              }
+              // if there is at least one face detected
+              if (biggestFace) {
+                log("Width: " + biggestFace.x)
+                log("Height: " + biggestFace.y)
+                // write the ellipse to the stream
+                im.ellipse(biggestFace.x + biggestFace.width / 2, biggestFace.y + biggestFace.height/2, biggestFace.width/2, biggestFace.height/2);
+                // and render it
+                win.show(im);
+                // count down frames with no face
+                missCounter = MISSES;
+                // attempt to correct position
+                correct(biggestFace, im);
+                // if the face's height/width > 1/6 the total image height/width 
+                if (biggestFace.height > im.height() / 9 || biggestFace.width > im.width()/9) {
+                  // halt movement
+                  client.stop();
+                  client.after(100, function() {
+                    // back up real quick
+                    client.back(.25);
+                  }).after(500, function() {
+                    // halt movement
+                    client.stop();
+                  });
+                }
+                // else, face isn't that close, CHASE!!
+                else {
+                  client.stop();
+                  client.front(.1);
+                }
+              }
+              // if we haven't seen a face for 10 consecutive frames
+              else if (missCounter == 0) {
+                // search
+                if (!searching) {
+                    console.log("calling search");
+                    srch();
+                }
+              }
+              // we still have hope we'll find that face again
+              else {
+                // decrement the counter
+                missCounter--;
+              }
+              // done processing
+              processingImage = false;
+
+            }, opts.scale, opts.neighbors, opts.min && opts.min[0], opts.min && opts.min[1]);
+          });
+        }
+      }
+    };
+
+    // search for more faces to react to
+    var srch = function() {
+      //halt movement
+      client.stop();
+      //tell the rest that we're searching
+      searching = true;
+      log('searching');
+      //rotate clockwise, then counterclockwise
+      client.counterClockwise(.25);
+      client.after(500, function() {
+        this.stop();
+        //this.counterClockwise(.35);
       });
-    }
-  }
-};
+    };
 
-// search for more faces to react to
-var srch = function() {
-  //halt movement
-  client.stop();
-  //tell the rest that we're searching
-  searching = true;
-  log('searching');
-  //rotate clockwise, then counterclockwise
-  client.clockwise(.35);
-  client.after(200, function() {
-    client.stop();
-    client.counterClockwise(.35);
-  });
-};
-
-// function to attempt and center face on the current and
-// subsequent frames
-var correct = function(face, im) {
-  // center of x-axis in pixels
-  var faceCenterX = face.width / 2;
-  // center of y-axis in pixels
-  var faceCenterY = face.height / 2;
-  // if face right of center
-  if (faceCenterX > im.width() / 2) {
-    client.clockwise(.25);
-    client.after(500, function() {
-      client.stop();
-    });
-  }
-  //if face left of center
-  else if (faceCenterX < im.width() / 2) {
-    client.counterClockwise(.25);
-    client.after(500, function() {
-      client.stop();
-    });
-  }
-  //if face is above center
-  if (faceCenterY > im.height() / 2) {
-    client.up(.25);
-    client.after(500, function() {
-      client.stop();
-    });
-  }
-  //if face is below center
-  else if (faceCenterY > im.height() / 2) {
-    client.down(.25);
-    client.after(500, function() {
-      client.stop();
-    });
-  }
-};
-
+    // function to attempt and center face on the current and
+    // subsequent frames
+    var correct = function(face, im) {
+      // center of x-axis in pixels
+      var faceCenterX = face.x + (face.width / 2);
+      // center of y-axis in pixels
+      var faceCenterY = face.y + (face.height / 2);
+      // if face right of center
+      if (faceCenterX > im.width() / 2) {
+        log("Rotating Clockwise");
+        client.stop();
+        client.clockwise(.25);
+        client.after(500, function() {
+          client.stop();
+        });
+      }
+      //if face left of center
+      else if (faceCenterX < im.width() / 2) {
+        log("Rotating Counter-Clockwise");
+        client.stop();
+        client.counterClockwise(.25);
+        client.after(500, function() {
+          client.stop();
+        });
+      }
+      //if face is above center
+      if (faceCenterY > im.height() / 2) {
+        log("Descending");
+        client.down(.25);
+        client.after(500, function() {
+          client.stop();
+        });
+      }
+      //if face is below center
+      else if (faceCenterY < im.height() / 2) {
+        log("Ascending");
+        client.up(.25);
+        client.after(500, function() {
+          client.stop();
+        });
+      }
+    };
+});
 
 
 app.post('/displaypngstream', function(req, res) {
@@ -262,7 +277,7 @@ app.post('/displaytcpstream', function(req, res) {
 });
 
 app.post('/filtercolor', function(req, res) {
-    var pngStream = drone.getPngStream();
+    var pngStream = client.getPngStream();
     var originalWindow = new cv.NamedWindow('Original', 1000);
     var redFilterWindow = new cv.NamedWindow('Red Filtered', 1000);
     var blueFilterWindow = new cv.NamedWindow('Blue Filtered', 1000);
@@ -272,7 +287,6 @@ app.post('/filtercolor', function(req, res) {
     pngStream
         .on('error', console.log)
         .on('data', function(pngBuffer) {
-            //console.log("got image");
             lastPng = pngBuffer;
 
         cv.readImage(lastPng, function(err, im) {
@@ -287,7 +301,7 @@ app.post('/filtercolor', function(req, res) {
             var lowCannyThresh = 0;
             var highCannyThresh = 100;
             var nIters = 2;
-            var maxArea = 2500;
+            var minArea = 100;
 
             // colors for the lines
             var GREEN = [0, 255, 0]; // B, G, R
@@ -318,7 +332,7 @@ app.post('/filtercolor', function(req, res) {
 
 
             var blueContours = filterBlue.findContours();
-            var redContours = filterRed.findContours()
+            var redContours = filterRed.findContours();
 
             // Access vertex data of contours
             var biggestBlueContour = 1;
@@ -334,8 +348,8 @@ app.post('/filtercolor', function(req, res) {
                 }
             }
             console.log(redContours.area(biggestRedContour));
-            blueContours.convexHull(biggestBlueContour, false);
-            blueContours.convexHull(biggestRedContour, false);
+            blueContours.boundingRect(biggestBlueContour, false);
+            blueContours.boundingRect(biggestRedContour, false);
 
             filterBlue.drawContour(blueContours, biggestBlueContour, RED);
             filterBlue.drawContour(redContours, biggestRedContour, RED);
